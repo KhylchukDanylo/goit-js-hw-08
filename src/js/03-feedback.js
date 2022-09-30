3248a69f5c74069b6b4183c6991292bf32880a0f
Danilo <hilcukdanilo@gmail.com>
2022-09-30 20:22:29 +0200

    add task 2 and 3(80%)

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,35 @@
-console.log('123');
+const throttle = require('lodash.throttle');
+
+const form = document.querySelector('form');
+const btnSubmit = form.elements[2];
+const valEmail = form.elements.email;
+const valMessage = form.elements.message;
+
+const userData = { email: '', message: '' };
+
+const saveData = throttle(function () {
+  localStorage.setItem('feedback-form-state', JSON.stringify(data));
+}, 500);
+
+form.addEventListener('keyup', onEmail);
+function onEmail() {
+  userData.email = valEmail.value;
+  userData.message = valMessage.value;
+}
+const sevedUserDate = localStorage.getItem('feedback-form-state');
+const parsedUserDate = JSON.parse(sevedUserDate);
+
+valEmail.value = parsedUserDate.email;
+valMessage.value = parsedUserDate.message;
+
+btnSubmit.addEventListener('click', onClickSubmit);
+function onClickSubmit(e) {
+  e.preventDefault();
+  console.log(parsedUserDate);
+  console.log(userData);
+
+  localStorage.setItem('feedback-form-state', JSON.stringify(userData));
+
+  valEmail.value = ' ';
+  valMessage.value = ' ';
+}
